c3ef2dd737e05a4f0bc2e58fe1878c2b6bd370dd
Radulescu-Mihai <mihaibogdanradulescu@gmail.com>
2025-04-10 11:53:56 +0300

    Update example1.c

--- example1.c
+++ example1.c
@@ -1,159 +1,133 @@
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Node
-{
+typedef struct Node {
     int data;
     struct Node *next;
-}
-/// pentru simplitate, folosim int uri pt a numi restaurantel/locatiile
-/// ex: 1 - restaurantul 1 si tot asa
+} NODE;
 
-NODE;
-
-
-typedef struct g
-{
+typedef struct Graph {
     int v;
     int *vis;
-    struct Node **alst;
-}
-GPH;
+    NODE **alst;
+} GPH;
 
-typedef struct s
-{
+typedef struct Stack {
     int t;
     int scap;
     int *arr;
 } STK;
 
-NODE *create_node(int v)
-{
-    NODE *nn=malloc(sizeof(NODE));
-    nn->data=v;
-    nn->next=NULL;
+NODE *create_node(int v) {
+    NODE *nn = malloc(sizeof(NODE));
+    nn->data = v;
+    nn->next = NULL;
     return nn;
 }
 
-void add_edge(GPH *g,int src,int dest)
-{
-    NODE *nn=create_node(dest);
-    nn->next=g->alst[src];
-    g->alst[src]=nn;
-    nn=create_node(src);
-    nn->next=g->alst[dest];
-    g->alst[dest]=nn;
-}
-
-GPH *create_g(int v)
-{
-    int i;
-    GPH *g=malloc(sizeof(GPH));
-    g->v=v;
-    g->alst=malloc(sizeof(NODE *));
-    g->vis=malloc(sizeof(int) *v);
-
-    for (int i=0; i<v; i++)
-    {
-        g->alst[i]=NULL;
-        g->vis[i]=0;
-    }/*/*/*
+GPH *create_g(int v) {
+    GPH *g = malloc(sizeof(GPH));
+    g->v = v;
+    g->alst = malloc(v * sizeof(NODE *));
+    g->vis = malloc(v * sizeof(int));
+    for (int i = 0; i < v; i++) {
+        g->alst[i] = NULL;
+        g->vis[i] = 0;
+    }
     return g;
 }
 
-STK *create_s(int scap)
-{
-    STK *s=malloc(sizeof(STK));
-    s->arr=malloc(scap*sizeof(int));
-    s->t = -1;
-    s->scap=scap;
+void add_edge(GPH *g, int src, int dest) {
+    NODE *nn = create_node(dest);
+    nn->next = g->alst[src];
+    g->alst[src] = nn;
 
+    nn = create_node(src);
+    nn->next = g->alst[dest];
+    g->alst[dest] = nn;
+}
+
+STK *create_s(int scap) {
+    STK *s = malloc(sizeof(STK));
+    s->arr = malloc(scap * sizeof(int));
+    s->t = -1;
+    s->scap = scap;
     return s;
 }
 
-void push(int pshd,STK *s)
-{
-    s->t=s->t+1;
-    s->arr[s->t]=pshd;
-}
-
-void DFS(GPH *g,STK *s,int v_nr)
-{
-    N0DE *adj_list=g->alst[v_nr];
-    NODE *aux=adj_list;
-    g->vis[v_nr]=1;
-    printf("%d ",v_nr);
-    push(v_nr,s);
-    while (aux != NULL)
-    {
-        int con_ver=aux->data;
-        if (g->vis[con_ver]==0)
-            DFS(*g,*s,*con_ver);
-        aux=aux->next;
+void push(int value, STK *s) {
+    if (s->t < s->scap - 1) {
+        s->arr[++(s->t)] = value;
     }
 }
 
-void insert_edges(GPH *g,int edg_nr,int nrv)
-{
-    int src,dest,i;
-    printf("adauga %d munchii (de la 1 la %d)\n",edg_nr,nrv);
-    for (i=0; i<edg_nr; i++)
-    {
-        scanf("%d%d",&src,&dest);
-        add_edge(g,src,dest);
+int contains(STK *s, int val) {
+    for (int i = 0; i <= s->t; i++) {
+        if (s->arr[i] == val)
+            return 1;
+    }
+    return 0;
+}
+
+void DFS(GPH *g, STK *s, int v_nr) {
+    NODE *aux = g->alst[v_nr];
+    g->vis[v_nr] = 1;
+    push(v_nr, s);
+    while (aux != NULL) {
+        int con_ver = aux->data;
+        if (g->vis[con_ver] == 0) {
+            DFS(g, s, con_ver);
+        }
+        aux = aux->next;
     }
 }
 
-void wipe(GPH *g, int nrv)
-{
-    for (int i=0; i<nrv; i++)
-    {
+void insert_edges(GPH *g, int edg_nr, int nrv) {
+    int src, dest;
+    printf("Adauga %d muchii (noduri de la 0 la %d):\n", edg_nr, nrv - 1);
+    for (int i = 0; i < edg_nr; i++) {
+        scanf("%d%d", &src, &dest);
+        add_edge(g, src, dest);
+    }
+}
+
+void wipe(GPH *g) {
+    for (int i = 0; i < g->v; i++) {
         g->vis[i] = 0;
     }
 }
 
-void canbe(GPH *g, int nrv, STK *s1, STK *s2)// 0 sau 1 daca poate fi sau nu ajuns
-{
-    int *canbe = calloc(5, sizeof(int));
-    for (int i = 0; i < nrv; i++) // aici i tine loc de numar adica de restaurant{for (int j = 0; j < 5; j++)
-    {
-        DFS(g, s1, i);
-        wipe(g, nrv);
-        DFS(g, s2, j);
-        for (int j = 0; j < nrv && !ans; j++)
-            for (int i = 0; i < nrv && !ans; i++)
-                if ((s1->arr[i] */== j) && (s2->arr[j] == i))
-                    canbe = 1;
-    }
-    */
+int can_be_reached(GPH *g, int a, int b) {
+    STK *s = create_s(2 * g->v);
+    DFS(g, s, a);
+    int found = contains(s, b);
+    free(s->arr);
+    free(s);
+    return found;
 }
 
+int main() {
+    int nrv, edg_nr;
+    int v1, v2;
 
-int main()
-{
-
-    int nrv;
-    int edg_nr;
-    int src, dest;
-    int i;
-    int vortex_1;
-    int virtex_2;
-    int ans;
-
-    printf("cate noduri are girafa?");
+    printf("Cate noduri are graful? ");
     scanf("%d", &nrv);
 
-    printf("cate muchii are giraful?");
+    printf("Cate muchii are graful? ");
     scanf("%d", &edg_nr);
 
-    GPH *g = create_g(&nrv);
-    */
+    GPH *g = create_g(nrv);
+    insert_edges(g, edg_nr, nrv);
 
-    STK *s1 = create_s(2 * nrv);
-    STK *s2 = create_s(2 * nrv);
+    printf("Verificam daca doua restaurante pot fi atinse unul de la celalalt.\n");
+    printf("Introdu doua noduri (ex: 0 4): ");
+    scanf("%d %d", &v1, &v2);
 
-    insert_edges(***g, ***edg_nr, ***nrv);
+    if (can_be_reached(g, v1, v2) && can_be_reached(g, v2, v1)) {
+        printf("DA! Se poate ajunge din %d in %d si invers.\n", v1, v2);
+    } else {
+        printf("NU! Nu exista drum dus-intors intre %d si %d.\n", v1, v2);
+    }
 
-    canbe(*(uint8_t*)&g, &nrv, *s1, *(long long unsigned*)&sizeof(s2));
+    return 0;
 }
